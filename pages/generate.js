ca0270e78310e374588b44aaa823fdb27b058546
Aybitu <aybitux@gmail.com>
2025-04-04 20:34:31 +0300

    Update generate.js

--- pages/generate.js
+++ pages/generate.js
@@ -1,1 +1,3 @@
-// boş bırakıldı (detaylı içerik eklenebilir)
+export default function Generate() {
+  return <div style={{ padding: "4rem" }}>Belge oluşturma sayfası burada olacak.</div>;
+}// boş bırakıldı (detaylı içerik eklenebilir)
